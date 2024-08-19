2a3748a98e9f641ffee1014c07daf524cddae997
Togji-TL <tagbial@hotmail.fr>
2024-08-19 20:02:42 +0200

    MAJ About et expériences

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import '../assets/styles/About.css';
-import profil from '../images/profil_lkdn.jpg';
+import image from '../images/about.jpg'
 import cv from '../CV_Togji_TAGBIA_LOUEVIE.pdf'
 
 const About = () => {
@@ -9,12 +9,12 @@ const About = () => {
             <div className="about-section">
                 <div className="about-text">
                     <h2>À propos</h2>
-                    <p>Fascinée par la science et son application pratique, c’est très tôt que j’ai décidé de m’orienter en informatique. J'ai vite découvert que ce domaine proposait une vaste quantité de branches tout aussi intéressantes les unes que les autres.</p>
+                    <p>Fascinée par la science et son application pratique, c’est très tôt que j’ai décidé de m’orienter vers le domaine informatique. J'ai vite découvert que ce monde proposait une vaste quantité de branches tout aussi intéressantes les unes que les autres.</p>
                     <p>J’ai d'abord décidé de m’orienter vers une formation générale et de renommée à l’EPITA (Ecole Pour l’Informatique et les Techniques Avancées), où je suis restée un an. Par la suite, j’ai décidé d’entamer un voyage à la découverte des possibilités qui s’offraient à moi dans un domaine en perpétuelle évolution.</p>
                     <p>Aujourd'hui, j'ai décidé de faire de la Data mon domaine de prédilection. C'est dans ce but que je me forme personnellement aux outils et techniques de la Data Analyse avant de pouvoir approfondir mes connaissances au sein de l'IA School pour les deux prochaines années.</p>
                 </div>
                 <div className="about-image">
-                    <img src={ profil } alt="profil"/>
+                    <img src={ image } alt="about-image"/>
                 </div>
             </div>
             <div className="skills-title" id="skills">
@@ -38,23 +38,34 @@ const About = () => {
                 </div>
             </div>
 
-            <h3 id="experience">Expérience</h3>
-            <div className="timeline">
-                <div className="timeline-item">
-                    <div className="timeline-date">2024 - Présent</div>
-                    <div className="timeline-content">
-                        <h4>IA School</h4>
-                        <p>Parcours Data Analyst</p>
+            < div className="timeline-container" id="experience">
+                <div className="timeline">
+                    <div className="timeline-item">
+                        <div className="timeline-box">
+                            <div className="timeline-date">2021 - 2023</div>
+                            <div className="timeline-content">
+                                <h4>Linedata Services</h4>
+                                <p>Missions</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="timeline-item">
+                        <div className="timeline-box">
+                            <div className="timeline-date">2020 - 2021</div>
+                            <div className="timeline-content">
+                                <h4>Veolia Water Technologies</h4>
+                                <p>Missions</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="timeline-item">
-                    <div className="timeline-date">2022 - 2024</div>
-                    <div className="timeline-content">
-                        <h4>Université Paris-Saclay</h4>
-                        <p>Spécialité Ingénierie Web</p>
-                    </div>
+            <div className="extra-content">
+                    <h3>Expérience</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    {/* <img src={ profil } alt="Example" style={{ width: '100%', borderRadius: '8px' }} /> */}
                 </div>
             </div>
+
             <div className="skills-button">
                 <a href= { cv } target="_blank" className="btn-cv" rel="noreferrer">En savoir plus</a>
             </div>
